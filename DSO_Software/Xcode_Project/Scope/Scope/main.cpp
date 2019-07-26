7bf879504606335f6030fcafc2ce28c2a7118c78
Daniel Vasile <dv_k98@hotmail.com>
2019-07-26 12:40:11 -0400

    Added Software Folder and Project

---
+++ DSO_Software/Xcode_Project/Scope/Scope/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Scope
+//
+//  Created by Daniel Vasile on 2019-07-26.
+//  Copyright © 2019 EEVengers. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
