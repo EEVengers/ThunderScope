b1c0354f7bb2e891a4c66d859e8c58fc3c16fc7c
Aleksa Bjelogrlic <abjelogr@edu.uwaterloo.ca>
2019-09-15 04:16:32 -0400

    Reduced FPGA resource requirements from 38 slices to 18. Now safely ignores any additional 4 byte packets until serial data is sent.

--- DSO_Firmware/Serial_Output_Test/FT2_Read.v
+++ DSO_Firmware/Serial_Output_Test/FT2_Read.v
@@ -25,20 +25,22 @@ assign rd_n_out = rxf_n_in | read_data;
 
 always @(posedge clk) 
 begin
- //read_data is inverted rd_n signal delayed by two cycles
- rd_delay <= {rd_delay[2:0],~rd_n_out};
- if (rd_stb)
- begin
- counter <= counter + 1'b1;
+ 
+ rd_delay <= {rd_delay[2:0],~rd_n_out}; //read_data is inverted rd_n signal delayed by two cycles
+ 
+ if (rd_stb) begin
+	counter <= counter + 1'b1;
 	case (counter)
 		2'b00 : d_out[31:24] <= d_in[7:0];
 		2'b01 : d_out[23:16] <= d_in[7:0];
 		2'b10 : d_out[15:8] <= d_in[7:0];
 		2'b11 : d_out[7:0] <= d_in[7:0];
 	endcase
-	//d_ready is when counter is about to overflow back to 0
-	d_ready <= &counter;
+	d_ready <= &counter; //d_ready asserted for one cycle when counter is about to overflow back to 0
  end
+ else
+	d_ready <= 1'b0;
+	
 end 
 
 endmodule
--- DSO_Firmware/Serial_Output_Test/Serial_Output_Test.v
+++ DSO_Firmware/Serial_Output_Test/Serial_Output_Test.v
@@ -4,66 +4,64 @@ module Serial_Output_Test(
 	 input[7:0] ft2_d_in,
 	 output ft2_rd_n_out,
 	 output ft2_wr_n_out,
-	 output ser_out,
-	 output ser_clk,
+	 output reg ser_out,
+	 output reg ser_clk,
 	 output [3:0] led
 	 );
+
+
+parameter 			IDLE = 1'b0,
+						SEND = 1'b1;
+
+reg state;
+reg[31:0] piso;
+reg[5:0] bit_counter;
+reg[1:0] clk_counter; 
 
 wire[31:0] d_out;
 wire d_ready;
 
-reg d_ready_q;
-reg[31:0] piso;
-reg[5:0] bit_counter;
-reg[1:0] clk_counter; 
-reg clk_stb;
-
-//Serial clock enabled when data begins to transmit 
-//Disabled when we transmit the correct number of bits 
-assign ser_clk_en = d_ready_q & ~bit_counter[5];
-
-//Mux to turn on or off serial clock based on enable signal
-//Serial clock is system clock divided by 4
-assign ser_clk = (ser_clk_en) ? (clk_counter[1]) : (1'b0);
-
-//Data is last bit of shift register
-assign ser_out = piso[31];
-
-//AND signal with delayed signal for one clock cycle load pulse
-assign d_load = d_ready & ~d_ready_q;
-
 //Debug LEDs
-assign led = bit_counter[3:0];
+assign led[3:0] = bit_counter[3:0];
 
 always @ (posedge clk_32)
 begin
-	
-	//Increment clk counter
-	clk_counter <= clk_counter + 1'b1;
-	//Generate clk strobe signal once per counter cycle (/4)
-	clk_stb <= (clk_counter==2'b00);
-	
-	if(clk_stb)
-	begin
-		//Flop delay for load pulse
-		d_ready_q <= d_ready;
-		
-		//PISO shift register & counter for number of bits sent
-		if (d_load)
-		begin
-			piso <= d_out;
-			bit_counter <= 6'b000000;
-		end
-		else if (ser_clk_en)
-		begin
-			bit_counter <= bit_counter + 1'b1;
-			piso <= {piso[30:0],1'b0}; 
-		end
-		else
-			piso <= 32'h00000000;
-	end
-	
+	case (state)
+		IDLE:
+			if (d_ready) begin
+				state <= SEND;
+				piso <= d_out;
+				bit_counter <= 6'b000000;
+				clk_counter <= 2'b00;
+			end
+			else
+				state <= IDLE;
+		SEND: 
+			begin
+				clk_counter <= clk_counter + 1'b1;	
+				
+				if (bit_counter[5] & clk_counter==2'b00) begin
+					state <= IDLE;
+					ser_out <= 1'b0;
+				end 
+				
+				else begin
+					state <= SEND;
+					ser_clk <= ~clk_counter[1];
+					
+					if (clk_counter==2'b00) begin
+						piso <= {piso[30:0],1'b0}; 
+						ser_out <= piso[31];
+						
+					end
+					else if (clk_counter==2'b10)
+						bit_counter <= bit_counter + 1'b1;
+
+				end			
+			end		
+	endcase
 end
+
 
 //Call FT2_Read as a submodule
 FT2_Read R1 (
