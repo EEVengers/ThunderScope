340366bce992ff3e0a224d85606d9994434966e3
Ratan Varghese <ratanv@protonmail.com>
2020-06-11 13:18:36 -0400

    basic chart

--- Software/waveview/src/App.tsx
+++ Software/waveview/src/App.tsx
@@ -1,23 +1,49 @@
+
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  XYPlot,
+  XAxis,
+  YAxis,
+  HorizontalGridLines,
+  VerticalGridLines,
+  LineSeries
+} from 'react-vis';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <div>
+        <div className="left_col">
+             <XYPlot width={300} height={300}>
+              <HorizontalGridLines style={{stroke: '#B7E9ED'}} />
+              <VerticalGridLines style={{stroke: '#B7E9ED'}} />
+              <XAxis
+                title="X Axis"
+                style={{
+                  line: {stroke: '#ADDDE1'},
+                  ticks: {stroke: '#ADDDE1'},
+                  text: {stroke: 'none', fill: '#6b6b76', fontWeight: 600}
+                } as any}
+              />
+              <YAxis title="Y Axis" />
+              <LineSeries
+                className="third-series"
+                data={[{x: 1, y: 3}, {x: 2, y: 5}, {x: 3, y: 15}, {x: 4, y: 12}]}
+                style={{
+                  strokeLinejoin: 'round',
+                  strokeWidth: 4
+                } as any}
+              />
+            </XYPlot>
+        </div>
+        <div className="right_col">
+        </div>
+        
+      </div>
+      
       </header>
     </div>
   );
