b02ceab2740f0abda55bad481963579ab8f16768
Jason Bonnell <jbonnell@Jasons-MacBook-Pro.local>
2020-06-18 13:43:04 -0400

    added flex

--- Software/waveview/src/App.tsx
+++ Software/waveview/src/App.tsx
@@ -1,9 +1,7 @@
-
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import {
-  XYPlot,
+  FlexibleXYPlot,
   XAxis,
   YAxis,
   HorizontalGridLines,
@@ -15,35 +13,24 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <div>
-        <div className="left_col">
-             <XYPlot width={300} height={300}>
-              <HorizontalGridLines style={{stroke: '#B7E9ED'}} />
-              <VerticalGridLines style={{stroke: '#B7E9ED'}} />
-              <XAxis
-                title="X Axis"
-                style={{
-                  line: {stroke: '#ADDDE1'},
-                  ticks: {stroke: '#ADDDE1'},
-                  text: {stroke: 'none', fill: '#6b6b76', fontWeight: 600}
-                } as any}
-              />
-              <YAxis title="Y Axis" />
-              <LineSeries
-                className="third-series"
-                data={[{x: 1, y: 3}, {x: 2, y: 5}, {x: 3, y: 15}, {x: 4, y: 12}]}
-                style={{
-                  strokeLinejoin: 'round',
-                  strokeWidth: 4
-                } as any}
-              />
-            </XYPlot>
+        <div>
+          <div className="graph_view">
+              <FlexibleXYPlot>
+                <HorizontalGridLines style={{stroke: '#B7E9ED'}} />
+                <VerticalGridLines style={{stroke: '#B7E9ED'}} />
+                <XAxis
+                  title="X Axis"
+                />
+                <YAxis 
+                  title="Y Axis" 
+                />
+                <LineSeries
+                  className="data-series"
+                  data={[{x: 1, y: 3}, {x: 2, y: 5}, {x: 3, y: 15}, {x: 4, y: 12}]}
+                />
+              </FlexibleXYPlot>
+          </div>
         </div>
-        <div className="right_col">
-        </div>
-        
-      </div>
-      
       </header>
     </div>
   );
