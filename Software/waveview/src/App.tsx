b75f0dfef16a0914eca68ea8ec0bacfc3d287aa1
Ratan Varghese <19657319+ratanvarghese@users.noreply.github.com>
2020-06-18 13:50:06 -0400

    Jason's App

--- Software/waveview/src/App.tsx
+++ Software/waveview/src/App.tsx
@@ -9,7 +9,8 @@ import {
   HorizontalGridLines,
   VerticalGridLines,
   LineSeries,
-  MarkSeries
+  MarkSeries,
+  FlexibleXYPlot
 } from 'react-vis';
 
 var pulse_arr:number[] = [0,0,
@@ -242,36 +243,24 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <div>
-        <div className="left_col">
-             <XYPlot width={300} height={300}>
-              <HorizontalGridLines style={{stroke: '#B7E9ED'}} />
-              <VerticalGridLines style={{stroke: '#B7E9ED'}} />
-              <XAxis
-                title="X Axis"
-                style={{
-                  line: {stroke: '#ADDDE1'},
-                  ticks: {stroke: '#ADDDE1'},
-                  text: {stroke: 'none', fill: '#6b6b76', fontWeight: 600}
-                } as any}
-              />
-              <YAxis title="Y Axis" />
-              <MarkSeries
-                className="third-series"
-                //data={[{x: 1, y: 3}, {x: 2, y: 5}, {x: 3, y: 15}, {x: 4, y: 12}]}
-                data={pulse_obj_arr}
-                style={{
-                  strokeLinejoin: 'round',
-                  strokeWidth: 4
-                } as any}
-              />
-            </XYPlot>
+        <div>
+          <div className="graph_view">
+              <FlexibleXYPlot>
+                <HorizontalGridLines style={{stroke: '#B7E9ED'}} />
+                <VerticalGridLines style={{stroke: '#B7E9ED'}} />
+                <XAxis
+                  title="X Axis"
+                />
+                <YAxis 
+                  title="Y Axis" 
+                />
+                <MarkSeries
+                  className="data-series"
+                  data={pulse_obj_arr}
+                />
+              </FlexibleXYPlot>
+          </div>
         </div>
-        <div className="right_col">
-        </div>
-        
-      </div>
-      
       </header>
     </div>
   );
