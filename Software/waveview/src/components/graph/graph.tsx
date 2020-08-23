53fadcae77daaf795d8920b1153f987a287a2aa0
Ratan Varghese <19657319+ratanvarghese@users.noreply.github.com>
2020-08-23 18:09:33 -0400

    Display different subsets of data over time

--- Software/waveview/src/components/graph/graph.tsx
+++ Software/waveview/src/components/graph/graph.tsx
@@ -10,7 +10,50 @@ import {
   LineSeries
 } from 'react-vis';
 
-class Graph extends React.Component {
+interface IGraphProps {
+}
+
+interface IGraphState {
+  tickCount: number;
+  curData: any[];
+}
+
+//See https://stackoverflow.com/a/46987987
+//Stateful code adapted from https://reactjs.org/docs/state-and-lifecycle.html
+class Graph extends React.Component<IGraphProps,IGraphState> {
+  timerID: number = 0;
+
+  constructor(props: IGraphProps) {
+    super(props);
+    this.state = {tickCount: 0, curData: []};
+  }
+
+  componentDidMount() {
+    this.timerID = window.setInterval(
+      () => this.tick(),
+      100
+    )
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID)
+  }
+
+  tick() {
+    let tickCount = this.state.tickCount + 1;
+    let pointPerTick = 10;
+    let tickWindow = 100;
+    let i0 = Math.min(
+              Math.max(0, (tickCount-tickWindow)*pointPerTick),
+              data.length - (tickWindow*pointPerTick)
+            );
+    let i1 = Math.min(tickCount*pointPerTick, data.length);
+    this.setState({
+      tickCount: tickCount,
+      curData: data.slice(i0, i1)
+    })
+  }
+
   render() {
     return (
       <div className="graph_view">
@@ -25,7 +68,7 @@ class Graph extends React.Component {
           />
           <LineSeries
             className="data-series"
-            data={data}
+            data={this.state.curData}
             style={{fill:"none"}}
             color="yellow"
           />
