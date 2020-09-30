0508944ff40512ec538f8bf4b30293389649cc07
Jason Bonnell <jbonnell@Jasons-MacBook-Pro.local>
2020-09-30 18:03:28 -0400

    search, stop, single initial setup

--- Software/waveview/src/components/bottombar/subcomponents/channel.tsx
+++ Software/waveview/src/components/bottombar/subcomponents/channel.tsx
@@ -1,13 +1,13 @@
-import React, {CSSProperties} from 'react';
+import React from 'react';
 import './channel.css';
 
 function Channel(props: any) {
   return (
-  <div className={props.channelClass} style={{color:props.channelColor}}>
-    <label>
-      CH{props.channelNumber}: {props.voltsPerDiv}V/div, {props.voltageValue}{props.voltageUnit}, {props.measurementType}
-    </label>
-  </div>
+    <div className={props.channelClass} style={{color:props.channelColor}}>
+      <label>
+        CH{props.channelNumber}: {props.voltsPerDiv}V/div, {props.voltageValue}{props.voltageUnit}, {props.measurementType}
+      </label>
+    </div>
   )
 }
   
--- Software/waveview/src/components/sidebar/sidebar.tsx
+++ Software/waveview/src/components/sidebar/sidebar.tsx
@@ -1,11 +1,37 @@
 import React from 'react';
+import Search from './subcomponents/search';
+import LargeButton from './subcomponents/largebutton';
 import './sidebar.css';
 
 class SideBar extends React.Component {
+
+  buttonList: {color: string, className: string, text: string}[];
+
+  constructor(props: any) {
+    super(props);
+    this.buttonList = [
+      {color: "red", className: "StopButton", text: "Stop"},
+      {color: "yellow", className: "SingleButton", text: "Single"}
+    ]
+  }
+
   render() {
     return (
       <div className="SideBarComponent">
-        SideBar Component
+        <Search>
+
+        </Search>
+        {
+        this.buttonList.map((b) => {
+          return (
+            <LargeButton
+              buttonColor = {b.color}
+              buttonClassName = {b.className}
+              buttonText = {b.text}
+            />
+          )
+        })
+      }
       </div>
     )
   }
