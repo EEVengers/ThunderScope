bf026df4bf741ee6941c8c0efd87a6be105d51b7
Jason Bonnell <jbonnell@Jasons-MacBook-Pro.local>
2020-11-18 17:23:05 -0500

    added yellow arrow to graph

--- Software/waveview/src/components/graph/graph.tsx
+++ Software/waveview/src/components/graph/graph.tsx
@@ -19,7 +19,15 @@ interface IGraphProps {
 function Graph(props: IGraphProps) {
   return (
     <div className="graph_view">
-      <div className="graph_sidebar" />
+      <div 
+        className="graph_sidebar"
+      >
+        <p 
+          className = "graph_arrow"
+        >
+          &#x21b3;
+        </p>
+      </div>
       <FlexibleXYPlot
         yDomain={props.yDomain}
         xDomain={props.xDomain}
