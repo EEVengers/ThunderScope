4a4754618c77cdacf5d1f26e413ca1c4c0197410
Jason Bonnell <jbonnell@uwaterloo.ca>
2021-01-16 18:18:59 -0500

    added basic logic for trigger reducer/state

--- Software/waveview/src/components/bottombar/subcomponents/trigger.tsx
+++ Software/waveview/src/components/bottombar/subcomponents/trigger.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import './../../../css/bottombar/subscomponents/trigger.css';
 
-function Trigger() {
-  return (
-    <div className="TriggerStatus"> 
-      Trig:CH1, Mode:RisingEdge
-    </div>
-  )
+class Trigger extends React.Component<any, any> { 
+  render() {
+    return (
+      <div className="TriggerStatus"> 
+        Trig:CH{this.props.triggerWidget.triggerChannel}, Mode:{this.props.triggerWidget.triggerType}
+      </div>
+    )
+  }
 }
 
-export default Trigger;
+function mapStateToProps(state: { triggerWidget: any; }) {
+  return {
+    triggerWidget: state.triggerWidget
+  };
+}
+
+export default connect(mapStateToProps)(Trigger);
---
+++ Software/waveview/src/configuration/enums/triggerType.tsx
@@ -0,0 +1,6 @@
+enum TriggerType {
+  RisingEdge = "RisingEdge",
+  FallingEdge = "FallingEdge"
+}
+
+export default TriggerType;
