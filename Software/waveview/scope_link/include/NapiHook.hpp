7e44ff7971b76b07647eb2529703f3f5c090ab68
Alex Vandenberg <a.vandenberg0175@gmail.com>
2021-02-10 11:25:42 -0500

    CMakeLists.txt now supports an include dir
    header files are now in a seperate folder

--- Software/waveview/scope_link/include/NapiHook.hpp
+++ Software/waveview/scope_link/include/NapiHook.hpp
@@ -17,7 +17,7 @@ enum NapiCommands {
 
 namespace scope_link
 {
-    //defines except variables so they can be changed
+    // defines except variables so they can be changed
     int _num_of_packet_processer = 10;
 
     // PACKET STRUCTURE
@@ -29,22 +29,23 @@ namespace scope_link
     //  |               |               |               |                                                       |
     //  |_______________|_______________|_______________|_______________________________________________________|
     struct NapiPacket {
-        //The command that is to be executed
-        //this does not change from recieve to transmit
+        // The command that is to be executed
+        // This does not change from recieve to transmit
         uint16_t command;
-        //the ID of the command and packet. Used to ensure that the proper data ends up where it needs to be
-        //since multiple commands can be submitted in parallel and execution order is no guaranteed to be
-        //the same as submission order
-        //this does not change from recieve to transmit
+        // The ID of the command and packet. Used to ensure that the proper data ends up where it needs to be.
+        // Since multiple commands can be submitted in parallel and execution order is no guaranteed to be.
+        // The same as submission order.
+        // This does not change from recieve to transmit.
         uint16_t packetID;
-        //the length of data
+        // The length of data
         uint16_t dataSize;
-        //The relevant payload of the packet
-        //the contents of the payload will can change when c++ sends the data back to the javascript
-        //******IMPORTANT******
-        //When recieving a packet from javascript, always memcpy it's data into a malloc'd buffer in c++
-        //The pointer memory should stay internal to c++ at all times.
-        //when sending a packet to javascript that needs the data, memcpy the entire NapiPacket and let the NAPI callback take care of memory allocation
+        // The relevant payload of the packet.
+        // The contents of the payload will can change when c++ sends the data back to the javascript
+        // ******IMPORTANT******
+        // When recieving a packet from javascript, always memcpy it's data into a malloc'd buffer in c++
+        // The pointer memory should stay internal to c++ at all times.
+        // when sending a packet to javascript that needs the data, memcpy the
+        // entire NapiPacket and let the NAPI callback take care of memory allocation
         uint8_t* data;
     };
 
