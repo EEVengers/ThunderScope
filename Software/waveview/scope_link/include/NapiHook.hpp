5ab12c206d06a51cf1c56445dbca7a9c50319878
Alex Vandenberg <a.vandenberg0175@gmail.com>
2021-02-10 16:57:21 -0500

    Removed the scop_link namespace
    Its not necessesary as far as I can tell.
    namepaces are for when you have conflicting function names.

--- Software/waveview/scope_link/include/NapiHook.hpp
+++ Software/waveview/scope_link/include/NapiHook.hpp
@@ -15,205 +15,202 @@ enum NapiCommands {
     ConfigureScope
 };
 
-namespace scope_link
-{
-    // defines except variables so they can be changed
-    int _num_of_packet_processer = 10;
+// defines except variables so they can be changed
+int _num_of_packet_processer = 10;
 
-    // PACKET STRUCTURE
-    //   _______________________________________________________________________________________________________
-    //  |               |               |               |                                                       |
-    //  |    Command    |   PacketID    |   dataSize    |       Packet Data                                     |
-    //  |     16bits    |    16bits     |    16bits     |     However Many Bits as                              |
-    //  |     2Bytes    |    2Bytes     |    2Bytes     |     Defined by dataSize                               |
-    //  |               |               |               |                                                       |
-    //  |_______________|_______________|_______________|_______________________________________________________|
-    struct NapiPacket {
-        // The command that is to be executed
-        // This does not change from recieve to transmit
-        uint16_t command;
-        // The ID of the command and packet. Used to ensure that the proper data ends up where it needs to be.
-        // Since multiple commands can be submitted in parallel and execution order is no guaranteed to be.
-        // The same as submission order.
-        // This does not change from recieve to transmit.
-        uint16_t packetID;
-        // The length of data
-        uint16_t dataSize;
-        // The relevant payload of the packet.
-        // The contents of the payload will can change when c++ sends the data back to the javascript
-        // ******IMPORTANT******
-        // When recieving a packet from javascript, always memcpy it's data into a malloc'd buffer in c++
-        // The pointer memory should stay internal to c++ at all times.
-        // when sending a packet to javascript that needs the data, memcpy the
-        // entire NapiPacket and let the NAPI callback take care of memory allocation
-        uint8_t* data;
-    };
+// PACKET STRUCTURE
+//   _______________________________________________________________________________________________________
+//  |               |               |               |                                                       |
+//  |    Command    |   PacketID    |   dataSize    |       Packet Data                                     |
+//  |     16bits    |    16bits     |    16bits     |     However Many Bits as                              |
+//  |     2Bytes    |    2Bytes     |    2Bytes     |     Defined by dataSize                               |
+//  |               |               |               |                                                       |
+//  |_______________|_______________|_______________|_______________________________________________________|
+struct NapiPacket {
+    // The command that is to be executed
+    // This does not change from recieve to transmit
+    uint16_t command;
+    // The ID of the command and packet. Used to ensure that the proper data ends up where it needs to be.
+    // Since multiple commands can be submitted in parallel and execution order is no guaranteed to be.
+    // The same as submission order.
+    // This does not change from recieve to transmit.
+    uint16_t packetID;
+    // The length of data
+    uint16_t dataSize;
+    // The relevant payload of the packet.
+    // The contents of the payload will can change when c++ sends the data back to the javascript
+    // ******IMPORTANT******
+    // When recieving a packet from javascript, always memcpy it's data into a malloc'd buffer in c++
+    // The pointer memory should stay internal to c++ at all times.
+    // when sending a packet to javascript that needs the data, memcpy the
+    // entire NapiPacket and let the NAPI callback take care of memory allocation
+    uint8_t* data;
+};
 
-    //Init Code
-    int InitScopeLink();
-    //WRAPER
-    Napi::Number InitScopeLinkWrapper(const Napi::CallbackInfo& info);
+//Init Code
+int InitScopeLink();
+//WRAPER
+Napi::Number InitScopeLinkWrapper(const Napi::CallbackInfo& info);
 
-    //Handle NAPI Command: This function will decode an NAPI command and queue it up for execution
-    int HandleCommand(unsigned char* data, size_t size);
-    //WRAPPER
-    Napi::Number SendCommand(const Napi::CallbackInfo& info);
+//Handle NAPI Command: This function will decode an NAPI command and queue it up for execution
+int HandleCommand(unsigned char* data, size_t size);
+//WRAPPER
+Napi::Number SendCommand(const Napi::CallbackInfo& info);
 
-    //GetData:
-    unsigned char* GetData(size_t* packetSize);
-    //WRAPER
-    Napi::ArrayBuffer GetDataWrapper(const Napi::CallbackInfo& info);
+//GetData:
+unsigned char* GetData(size_t* packetSize);
+//WRAPER
+Napi::ArrayBuffer GetDataWrapper(const Napi::CallbackInfo& info);
 
-    //Stop Code
-    //void Stop();
-    //WRAPPER
-    //void StopWrapper(const Napi::CallbackInfo& info);
+//Stop Code
+//void Stop();
+//WRAPPER
+//void StopWrapper(const Napi::CallbackInfo& info);
 
-    //Test Throughput Code
-    unsigned char* TestThroughPut();
-    //WRAPER
-    Napi::ArrayBuffer TestThroughPutWrapper(const Napi::CallbackInfo& info);
+//Test Throughput Code
+unsigned char* TestThroughPut();
+//WRAPER
+Napi::ArrayBuffer TestThroughPutWrapper(const Napi::CallbackInfo& info);
 
-    //GetTimeNs
-    unsigned long GetTimeUs();
-    //WRAPPER
-    Napi::Number GetTimeUsWrapper(const Napi::CallbackInfo& info);
+//GetTimeNs
+unsigned long GetTimeUs();
+//WRAPPER
+Napi::Number GetTimeUsWrapper(const Napi::CallbackInfo& info);
 
-    //EXPORT
-    Napi::Object NapiExport(Napi::Env env, Napi::Object exports);
-    NODE_API_MODULE(addon,NapiExport);
+//EXPORT
+Napi::Object NapiExport(Napi::Env env, Napi::Object exports);
+NODE_API_MODULE(addon,NapiExport);
 
-    //empty packet declatration
-    uint8_t* emptyData;
-    NapiPacket _emptyPacket = { 0, 0, 0, emptyData };
+//empty packet declatration
+uint8_t* emptyData;
+NapiPacket _emptyPacket = { 0, 0, 0, emptyData };
 
-    //Class that will do all the requests
-    class PacketProcesser {
-        private:
+//Class that will do all the requests
+class PacketProcesser {
+    private:
 
-        //Queue References
-        std::queue<scope_link::NapiPacket*>& _txQueue;
-        std::queue<scope_link::NapiPacket*>& _rxQueue;
+    //Queue References
+    std::queue<NapiPacket*>& _txQueue;
+    std::queue<NapiPacket*>& _rxQueue;
 
-        //since each processer shares the smae tx and rx queue, this mutex is used to assure only 1 uses it at a time
-        std::shared_mutex& _txLock;
-        std::shared_mutex& _rxLock;
+    //since each processer shares the smae tx and rx queue, this mutex is used to assure only 1 uses it at a time
+    std::shared_mutex& _txLock;
+    std::shared_mutex& _rxLock;
 
-        std::thread _worker;
-        bool run;
-        scope_link::NapiPacket* _rxPacket;
-        scope_link::NapiPacket* _txPacket;
+    std::thread _worker;
+    bool run;
+    NapiPacket* _rxPacket;
+    NapiPacket* _txPacket;
 
-        //This is the looping function that the thread runs when start() is called
-        static void job(PacketProcesser* obj) {
-            while(obj->run) {
-                //get a packet
-                bool gotPacket = false;;
-                while(!gotPacket && obj->run) {
-                    obj->_rxLock.lock();
-                    if(obj->_rxQueue.empty()) {
-                        std::this_thread::sleep_for(std::chrono::milliseconds(1));
-                        obj->_rxLock.unlock();
-                        std::this_thread::sleep_for(std::chrono::milliseconds(9));
-                    } else {
-                        obj->_rxPacket = obj->_rxQueue.front();
-                        obj->_rxQueue.pop();
-                        gotPacket = true;
-                        obj->_rxLock.unlock();
-                    }
+    //This is the looping function that the thread runs when start() is called
+    static void job(PacketProcesser* obj) {
+        while(obj->run) {
+            //get a packet
+            bool gotPacket = false;;
+            while(!gotPacket && obj->run) {
+                obj->_rxLock.lock();
+                if(obj->_rxQueue.empty()) {
+                    std::this_thread::sleep_for(std::chrono::milliseconds(1));
+                    obj->_rxLock.unlock();
+                    std::this_thread::sleep_for(std::chrono::milliseconds(9));
+                } else {
+                    obj->_rxPacket = obj->_rxQueue.front();
+                    obj->_rxQueue.pop();
+                    gotPacket = true;
+                    obj->_rxLock.unlock();
                 }
-                //check that we got a packet, not that the thread was told to stop
-                if(!obj->run)
-                    break;
+            }
+            //check that we got a packet, not that the thread was told to stop
+            if(!obj->run)
+                break;
 
-                //Debug Printout
-                printf("Packet Recieved: PacketID: %X, Command: %d, dataSize: %d\n", obj->_rxPacket->packetID, obj->_rxPacket->command, obj->_rxPacket->dataSize);
+            //Debug Printout
+            printf("Packet Recieved: PacketID: %X, Command: %d, dataSize: %d\n", obj->_rxPacket->packetID, obj->_rxPacket->command, obj->_rxPacket->dataSize);
 
-                //create the txPacket
-                obj->_txPacket = (scope_link::NapiPacket*)malloc(sizeof(scope_link::NapiPacket));
-                obj->_txPacket->command = obj->_rxPacket->command;
-                obj->_txPacket->packetID = obj->_rxPacket ->packetID;
-                //run the packet's request
-                /*
-                 * The repsonsibility for each command that is called and excuted is to return the
-                 * packet data as a malloc'd pointer so that it can be sent to JS then freed
-                 *
-                 * Signature of runable commands should follow : 
-                 *      uint16_t FUNCTION(unsigned char* rxData, unsigned char** txData) :
-                 *      return: the size of txData
-                 *      rxData: the unsigned char that holds data sent by JS
-                 *      txData: a pointer to the data array that will be sent to JS (the called
-                 *              function must malloc the underlying data)
-                 */
-                switch(static_cast<NapiCommands>(obj->_rxPacket->command)) {
-                    case GetChannelData:
-                    {
-                        uint8_t* txData = (uint8_t*)malloc(sizeof(uint8_t) * 255);
-                        for(uint8_t i = 0; i <= 254; i++) {
-                            if(i % 2 == 0) {
-                                txData[i] = i / 2;
-                            } else {
-                                txData[i] = static_cast<uint8_t>((static_cast<uint32_t>(i) * 2) % 200);
-                            }
+            //create the txPacket
+            obj->_txPacket = (NapiPacket*)malloc(sizeof(NapiPacket));
+            obj->_txPacket->command = obj->_rxPacket->command;
+            obj->_txPacket->packetID = obj->_rxPacket ->packetID;
+            //run the packet's request
+            /*
+             * The repsonsibility for each command that is called and excuted is to return the
+             * packet data as a malloc'd pointer so that it can be sent to JS then freed
+             *
+             * Signature of runable commands should follow : 
+             *      uint16_t FUNCTION(unsigned char* rxData, unsigned char** txData) :
+             *      return: the size of txData
+             *      rxData: the unsigned char that holds data sent by JS
+             *      txData: a pointer to the data array that will be sent to JS (the called
+             *              function must malloc the underlying data)
+             */
+            switch(static_cast<NapiCommands>(obj->_rxPacket->command)) {
+                case GetChannelData:
+                {
+                    uint8_t* txData = (uint8_t*)malloc(sizeof(uint8_t) * 255);
+                    for(uint8_t i = 0; i <= 254; i++) {
+                        if(i % 2 == 0) {
+                            txData[i] = i / 2;
+                        } else {
+                            txData[i] = static_cast<uint8_t>((static_cast<uint32_t>(i) * 2) % 200);
                         }
-                        obj->_txPacket->data = txData;
-                        obj->_txPacket->dataSize = static_cast<uint16_t>(255);
-                    }//***** IF YOU GET error C2360 ---> Put your code in brackets, for some reason VC++ refuses to let you declare a variable in only 1 case unless its in a different scope, hence the brackets   
-                    break;
-                    case GetChannelMeasurements:
-                    break;
-                    case ConfigureScope:
-                    break;
-                    default:
-                        std::cout << "Unknown Command: " << obj->_rxPacket->command << std::endl; 
-                    break;
-                }
-                //put the txPacket into the queue
-                obj->_txLock.lock();
-                obj->_txQueue.push(obj->_txPacket);
-                obj->_txLock.unlock();
+                    }
+                    obj->_txPacket->data = txData;
+                    obj->_txPacket->dataSize = static_cast<uint16_t>(255);
+                }//***** IF YOU GET error C2360 ---> Put your code in brackets, for some reason VC++ refuses to let you declare a variable in only 1 case unless its in a different scope, hence the brackets   
+                break;
+                case GetChannelMeasurements:
+                break;
+                case ConfigureScope:
+                break;
+                default:
+                    std::cout << "Unknown Command: " << obj->_rxPacket->command << std::endl; 
+                break;
             }
+            //put the txPacket into the queue
+            obj->_txLock.lock();
+            obj->_txQueue.push(obj->_txPacket);
+            obj->_txLock.unlock();
         }
+    }
 
-        public:
+    public:
 
-        //each thread will be given the rx queue and the tx queue. Once started
-        //they will take packets from the rxQueue (once aviable).
-        //process them, then put the return packet into the txQueue
-        PacketProcesser(std::queue<scope_link::NapiPacket*>& txQueue,
-                        std::queue<scope_link::NapiPacket*>& rxQueue,
-                        std::shared_mutex& txLock,
-                        std::shared_mutex& rxLock) : 
-            _txQueue(txQueue),
-            _rxQueue(rxQueue),
-            _txLock(txLock),
-            _rxLock(rxLock)
-        {
-        }
+    //each thread will be given the rx queue and the tx queue. Once started
+    //they will take packets from the rxQueue (once aviable).
+    //process them, then put the return packet into the txQueue
+    PacketProcesser(std::queue<NapiPacket*>& txQueue,
+                    std::queue<NapiPacket*>& rxQueue,
+                    std::shared_mutex& txLock,
+                    std::shared_mutex& rxLock) : 
+        _txQueue(txQueue),
+        _rxQueue(rxQueue),
+        _txLock(txLock),
+        _rxLock(rxLock)
+    {
+    }
 
-        void start() {
-            //stop the current worker if it is already running
-            if(_worker.joinable()) {
-                run = false;
-                _worker.join();
-            }
-
-            run = true;
-            _worker = std::thread(scope_link::PacketProcesser::job,this);
-        }
-
-        void stop() {
+    void start() {
+        //stop the current worker if it is already running
+        if(_worker.joinable()) {
             run = false;
-            if(_worker.joinable()) {
-                _worker.join();
-            }
+            _worker.join();
         }
 
-        ~PacketProcesser() {
-            if(_worker.joinable())
-                _worker.join();
+        run = true;
+        _worker = std::thread(PacketProcesser::job,this);
+    }
+
+    void stop() {
+        run = false;
+        if(_worker.joinable()) {
+            _worker.join();
         }
-    };
-}
-  
+    }
+
+    ~PacketProcesser() {
+        if(_worker.joinable())
+            _worker.join();
+    }
+};
+
 #endif
--- Software/waveview/scope_link/src/NapiHook.cpp
+++ Software/waveview/scope_link/src/NapiHook.cpp
@@ -6,29 +6,27 @@
 #include "string.h"
 
 //Queues for Rx and Tx between C++ and Js
-std::queue<scope_link::NapiPacket*> _txQueue;
-std::queue<scope_link::NapiPacket*> _rxQueue;
+std::queue<NapiPacket*> _txQueue;
+std::queue<NapiPacket*> _rxQueue;
 //Mutexs for the queues
 std::shared_mutex _txLock;
 std::shared_mutex _rxLock;
 
 //array of packet processers that will execute the requests from JS
-scope_link::PacketProcesser** _processers;
+PacketProcesser** _processers;
 
 //test variables
 EVSharedCache* dataCache;
 unsigned char* bigArray;
 
-
-
 //Init Code
-int scope_link::InitScopeLink() {
+int InitScopeLink() {
     
-    _processers = (scope_link::PacketProcesser**)malloc(sizeof(scope_link::PacketProcesser*)
-                  * scope_link::_num_of_packet_processer);
+    _processers = (PacketProcesser**)malloc(sizeof(PacketProcesser*)
+                  * _num_of_packet_processer);
 
-    for(int i = 0; i < scope_link::_num_of_packet_processer; i++) {
-        _processers[i] = new scope_link::PacketProcesser(_txQueue,_rxQueue,_txLock,_rxLock);
+    for(int i = 0; i < _num_of_packet_processer; i++) {
+        _processers[i] = new PacketProcesser(_txQueue,_rxQueue,_txLock,_rxLock);
         _processers[i]->start();
     }
 
@@ -42,15 +40,15 @@ int scope_link::InitScopeLink() {
 }
 
 //WRAPER
-Napi::Number scope_link::InitScopeLinkWrapper(const Napi::CallbackInfo& info) {
+Napi::Number InitScopeLinkWrapper(const Napi::CallbackInfo& info) {
     Napi::Env env = info.Env();
 
-    return Napi::Number::New(env,scope_link::InitScopeLink());
+    return Napi::Number::New(env,InitScopeLink());
 }
 
 
-int scope_link::HandleCommand(unsigned char* data, size_t size) {
-    scope_link::NapiPacket* packet = (scope_link::NapiPacket*)malloc(sizeof(scope_link::NapiPacket));
+int HandleCommand(unsigned char* data, size_t size) {
+    NapiPacket* packet = (NapiPacket*)malloc(sizeof(NapiPacket));
     
     packet->command = data[0];
     packet->packetID = ((uint32_t*)(data + 1))[0];
@@ -63,7 +61,7 @@ int scope_link::HandleCommand(unsigned char* data, size_t size) {
 
 //WRAPPER
 //Bad name i know, but this is a command that JS has sent, ie JS Send Command -> C++
-Napi::Number scope_link::SendCommand(const Napi::CallbackInfo& info) {
+Napi::Number SendCommand(const Napi::CallbackInfo& info) {
     
     Napi::Buffer<uint8_t> buffer = info[0].As<Napi::Buffer<uint8_t>>();
     uint8_t* data = (unsigned char*)malloc(sizeof(uint8_t) * buffer.Length());
@@ -71,21 +69,21 @@ Napi::Number scope_link::SendCommand(const Napi::CallbackInfo& info) {
 
     //push the packet into the queue
     _rxLock.lock();
-    _rxQueue.push((scope_link::NapiPacket*)data);
+    _rxQueue.push((NapiPacket*)data);
     _rxLock.unlock();
     
-    return Napi::Number::New(info.Env(),((scope_link::NapiPacket*)(data))->packetID);
+    return Napi::Number::New(info.Env(),((NapiPacket*)(data))->packetID);
 }
 
-unsigned char* scope_link::GetData(size_t* packetSize) {
+unsigned char* GetData(size_t* packetSize) {
     unsigned char* packetBuff;
-    scope_link::NapiPacket* packet;
+    NapiPacket* packet;
     
     // TODO here would go the code that checks that packet processing machine for any packets to
     // send to javascript. This is to be implmented in talks with Alex and Daniel
     _txLock.lock();
     if(_txQueue.empty()) {
-        packet = &scope_link::_emptyPacket;
+        packet = &_emptyPacket;
     } else {
         packet = _txQueue.front();
         _txQueue.pop();
@@ -106,12 +104,12 @@ unsigned char* scope_link::GetData(size_t* packetSize) {
     return packetBuff;
 }
 
-Napi::ArrayBuffer scope_link::GetDataWrapper(const Napi::CallbackInfo& info) {
+Napi::ArrayBuffer GetDataWrapper(const Napi::CallbackInfo& info) {
     Napi::Env env = info.Env();
     // packet size is the number of bytes the packet is
     size_t packetSize;
 
-    unsigned char* data = scope_link::GetData(&packetSize);
+    unsigned char* data = GetData(&packetSize);
     
     Napi::ArrayBuffer array =  Napi::ArrayBuffer::New(env,(void*)data,packetSize,
                                   [](Napi::Env env,void* buff){});
@@ -122,7 +120,7 @@ Napi::ArrayBuffer scope_link::GetDataWrapper(const Napi::CallbackInfo& info) {
 }
 
 // Test Throughput Code
-unsigned char* scope_link::TestThroughPut() {
+unsigned char* TestThroughPut() {
     unsigned char* data = (unsigned char*)malloc(sizeof(unsigned char) * TEST_ARRAY_SIZE);
     
     memcpy(data,bigArray,TEST_ARRAY_SIZE);
@@ -131,10 +129,10 @@ unsigned char* scope_link::TestThroughPut() {
 }
 
 // WRAPER
-Napi::ArrayBuffer scope_link::TestThroughPutWrapper(const Napi::CallbackInfo& info) {
+Napi::ArrayBuffer TestThroughPutWrapper(const Napi::CallbackInfo& info) {
     Napi::Env env = info.Env();
     
-    unsigned char* data = scope_link::TestThroughPut();
+    unsigned char* data = TestThroughPut();
     
     Napi::ArrayBuffer array = Napi::ArrayBuffer::New(env,
                                                      (void*)data,
@@ -149,26 +147,26 @@ Napi::ArrayBuffer scope_link::TestThroughPutWrapper(const Napi::CallbackInfo& in
 }
 
 //GetTimeUs
-unsigned long scope_link::GetTimeUs() {
+unsigned long GetTimeUs() {
     unsigned long us = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::
                       now().time_since_epoch()).count();
     return us;
 }
 
 //WRAPPER
-Napi::Number scope_link::GetTimeUsWrapper(const Napi::CallbackInfo& info) {
+Napi::Number GetTimeUsWrapper(const Napi::CallbackInfo& info) {
     Napi::Env env = info.Env();
     
-    return Napi::Number::New(env,scope_link::GetTimeUs());
+    return Napi::Number::New(env,GetTimeUs());
 }
 
-Napi::Object scope_link::NapiExport(Napi::Env env, Napi::Object exports) {
+Napi::Object NapiExport(Napi::Env env, Napi::Object exports) {
 
-    exports.Set("InitScopeLink",Napi::Function::New(env,scope_link::InitScopeLinkWrapper));
-    exports.Set("SendCommand",Napi::Function::New(env,scope_link::SendCommand));
-    exports.Set("GetData",Napi::Function::New(env,scope_link::GetDataWrapper));
-    exports.Set("GetTimeUs",Napi::Function::New(env,scope_link::GetTimeUsWrapper));
-    exports.Set("TestThroughPut",Napi::Function::New(env,scope_link::TestThroughPutWrapper));
+    exports.Set("InitScopeLink",Napi::Function::New(env,InitScopeLinkWrapper));
+    exports.Set("SendCommand",Napi::Function::New(env,SendCommand));
+    exports.Set("GetData",Napi::Function::New(env,GetDataWrapper));
+    exports.Set("GetTimeUs",Napi::Function::New(env,GetTimeUsWrapper));
+    exports.Set("TestThroughPut",Napi::Function::New(env,TestThroughPutWrapper));
 
     return exports;
 }
