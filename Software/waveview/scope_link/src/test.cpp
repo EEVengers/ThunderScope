0a67c4495a1218fc9afcc386396bb7c5c2e4cfdc
Daniel Vasile <dv_k98@hotmail.com>
2021-03-02 17:10:25 -0500

    Checkpoint for sockets

--- Software/waveview/scope_link/src/test.cpp
+++ Software/waveview/scope_link/src/test.cpp
@@ -1,14 +1,94 @@
 #include <unistd.h> 
-#include <stdio.h> 
-#include <sys/socket.h> 
+#include <stdio.h>
+
+#ifdef WIN32
+#include <WinSock2.h>
+#include <winsock.h>
+#else
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/un.h> 
+#endif
+
 #include <stdlib.h> 
-#include <netinet/in.h> 
 #include <string.h> 
-#include <sys/un.h>
 #include <cerrno>
 #include <string.h>
 
 #define BUFF_SIZE 1024
+
+//define the socket struct cause the include file seems to be MIA
+#ifdef WIN32
+#define UNIX_PATH_MAX 108
+typedef struct sockaddr_un
+{
+     ADDRESS_FAMILY sun_family;     /* AF_UNIX */
+     char sun_path[UNIX_PATH_MAX];  /* pathname */
+} SOCKADDR_UN, *PSOCKADDR_UN;
+#endif
+
+#ifdef WIN32
+int make_named_socket (const char *filename) {
+  
+  SOCKET sock = INVALID_SOCKET;
+  WSADATA wsaData;
+
+  int iResult = 0;// used to return function results
+
+  // The socket address to be passed to bind
+  sockaddr_un service;
+
+  //----------------------
+  // Initialize Winsock
+  iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+  if (iResult != NO_ERROR) {
+      wprintf(L"Error at WSAStartup()\n");
+      return 1;
+  }
+
+  if (LOBYTE(wsaData.wVersion) != 2 || HIBYTE(wsaData.wVersion) != 2) {
+    /* Tell the user that we could not find a usable */
+    /* WinSock DLL.                                  */
+    printf("Could not find a usable version of Winsock.dll\n");
+    WSACleanup();
+    return 1;
+  }
+  else
+    printf("The Winsock 2.2 dll was found okay\n");
+
+  //----------------------
+  // Create a SOCKET for listening for 
+  // incoming connection requests
+  sock = socket(AF_UNIX, SOCK_STREAM, 0);
+  if (sock == INVALID_SOCKET) {
+      wprintf(L"socket function failed with error: %u\n", WSAGetLastError());
+      WSACleanup();
+      return 1;
+  }
+  //----------------------
+  // The sockaddr_in structure specifies the address family,
+  service.sun_family = AF_UNIX;
+  for(int i = 0; i < 11; i++) {
+    service.sun_path[i] = filename[i];
+  }
+  service.sun_path[11] = '\0';
+  printf("FILEPATH: %s, COPIED FILEPATH: %s\n",filename,service.sun_path);
+  //----------------------
+  // Bind the socket.
+  iResult = bind(sock, (SOCKADDR *) &service, sizeof (service));
+  if (iResult == SOCKET_ERROR) {
+      wprintf(L"bind failed with error %u\n", WSAGetLastError());
+      closesocket(sock);
+      WSACleanup();
+      return 1;
+  }
+  else
+      wprintf(L"bind returned success\n");
+
+  return sock;
+}
+
+#else
 
 int make_named_socket (const char *filename)
 {
@@ -17,7 +97,10 @@ int make_named_socket (const char *filename)
   size_t size;
 
   /* Create the socket. */
-  sock = socket (PF_LOCAL, SOCK_STREAM, 0);
+  sock = socket (AF_UNIX, SOCK_STREAM, 0);
+
+  printf("Socket Created With FP: %d\n",sock);
+
   if (sock < 0)
   {
     perror ("socket");
@@ -25,7 +108,7 @@ int make_named_socket (const char *filename)
   }
 
   /* Bind a name to the socket. */
-  name.sun_family = AF_LOCAL;
+  name.sun_family = AF_UNIX;
   strncpy (name.sun_path, filename, sizeof (name.sun_path));
   name.sun_path[sizeof (name.sun_path) - 1] = '\0';
 
@@ -35,11 +118,13 @@ int make_named_socket (const char *filename)
      Alternatively you can just do:
      size = SUN_LEN (&name);
   */
-  size = (offsetof (struct sockaddr_un, sun_path)
-          + strlen (name.sun_path));
+  //size = (offsetof (struct sockaddr_un, sun_path)
+  //        + strlen (name.sun_path));
+  size = SUN_LEN (&name);
 
   if (bind (sock, (struct sockaddr *) &name, size) < 0)
     {
+      printf("Error code: %d\n",errno);
       perror ("bind");
       exit (EXIT_FAILURE);
     }
@@ -47,9 +132,72 @@ int make_named_socket (const char *filename)
   return sock;
 }
 
+#endif
+
+#ifdef WIN32
+
+void runClient(const char *filename) {
+
+  SOCKET sock = INVALID_SOCKET;
+  WSADATA wsaData;
+
+  int iResult = 0;// used to return function results
+
+  // The socket address to be passed to bind
+  sockaddr_un service;
+
+  // Initialize Winsock
+  iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+  if (iResult != NO_ERROR) {
+      wprintf(L"Error at WSAStartup()\n");
+      return;
+  }
+
+  sock = socket(AF_UNIX, SOCK_STREAM, 0);
+  if (sock == INVALID_SOCKET) {
+      wprintf(L"socket function failed with error: %u\n", WSAGetLastError());
+      WSACleanup();
+      return;
+  }
+
+  service.sun_family = AF_UNIX;
+  for(int i = 0; i < 11; i++) {
+    service.sun_path[i] = filename[i];
+  }
+  service.sun_path[11] = '\0';
+  printf("Trying to connect to unix socket: %s\n",service.sun_path);
+
+  if(0 != connect(sock, (SOCKADDR *) &service,13)) {
+    wprintf(L"Error on connect(): %d\n",WSAGetLastError());
+  }
+
+  char buff[BUFF_SIZE];
+  memcpy(buff,"HELLO WORLD", sizeof("HELLO WORLD"));
+
+  send(sock, buff, BUFF_SIZE, 0);
+
+  if(sock != -1) {
+    closesocket(sock);
+  }
+
+  WSACleanup();
+}
+
+#else
+
+void runClient() {
+
+}
+
+#endif
+
 int main(int argc, char const *argv[]) 
 {
-  char* socket_name = "/usr/local/temp/socket_test"; 
+  //unix filesystem socket path
+  //char* socket_name = "/usr/local/temp/socket_test";
+  //char* socket_name = "/tmp/socket_test";
+  //windows filesystem socket path
+  char* socket_name = "C:\\tmp\\sock"; 
   char buffer[BUFF_SIZE], sendBuff[BUFF_SIZE];
   int rc; 
   int server_fd, client_fd;
@@ -57,23 +205,35 @@ int main(int argc, char const *argv[])
   
   memcpy(sendBuff,"HELLO WORLD", sizeof("HELLO WORLD"));
 
+  if(argc > 2) {
+    runClient(socket_name);
+    return 1;
+  }
   if(argc > 1)
-      goto unlink;
+    goto cleanup;
 
   do {
     //open a AF_LOCAL socket using the GNU expample
     server_fd = make_named_socket(socket_name);
   
     //listen on the socket for any clients
-    rc = listen(server_fd,1);
-    if(rc < 0) {
-      perror("listen() failed");
-      printf("Error code: %d",errno);
+    printf("Ready for client connect().\n");
+
+    rc = listen(server_fd,10);
+    if(rc != 0) {
+      perror("listen() failed\n");
+      #ifdef WIN32
+      printf("Error code: %d\n",WSAGetLastError());
+      #else
+      printf("Error code: %d\n",errno,rc);
+      #endif
+
       break;
     }
-    printf("Ready for client connect().\n");
-  
+    printf("Client found, accepting...\n");
+
     //accept the first client that connected
+    
     rc = client_fd = accept(server_fd,NULL,NULL);
     if(rc < 0) {
       perror("accept() failed");
@@ -91,7 +251,7 @@ int main(int argc, char const *argv[])
         break;
       }
       if(rc == 0 || rc < sizeof(buffer)) {
-        printf("Client has closed gracefully before all the data was sent");
+        printf("Client has closed gracefully before all the data was sent\n");
         done = true;
         break;
       }
@@ -100,24 +260,42 @@ int main(int argc, char const *argv[])
       send(client_fd,sendBuff,sizeof(sendBuff),0);
       i++;
     }
-    printf("Got %i Packets from the client and send back %i replies",i,i);
+    printf("Got %i Packets from the client and send back %i replies\n",i,i);
     printf("Tranmission Complete....\n");
   
   }while(0);
   
   if(server_fd != -1) {
+    #ifdef WIN32
+    closesocket(server_fd);
+    #else
     close(server_fd);
+    #endif
     printf("Closed server_fd socket\n");
   }
 
   if(client_fd != -1) {
+    #ifdef WIN32
+    closesocket(client_fd);
+    #else
     close(client_fd);
+    #endif
     printf("Closed client_fd socket\n");
   }
   
-unlink:
+cleanup:
+  #ifdef WIN32
   printf("Unlinking: %s\n",socket_name);
   unlink(socket_name);
+  #else
+  printf("Unlinking: %s\n",socket_name);
+  unlink(socket_name);
+  #endif 
+
+  #ifdef WIN32
+  printf("Freeing program from windows sockets\n");
+  WSACleanup();
+  #endif
 
   return 0;
 }
