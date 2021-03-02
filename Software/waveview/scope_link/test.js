cd5337cc0eeb452bac0c454810eaf61f5d282d4c
Daniel Vasile <dv_k98@hotmail.com>
2021-03-02 17:10:25 -0500

    Checkpoint for sockets

--- Software/waveview/scope_link/test.js
+++ Software/waveview/scope_link/test.js
@@ -1,10 +1,20 @@
-const net = require('net');
-require('buffer').Buffer;
+const { lstat } = require("fs");
+const net = require("net");
 
 var RANDOMSTRING = "qqqmakctozqcikswtsoeszjfbeagpyqpidecwwzlepydibhzfxqyhygyorjswfwotsrjbmaqbssxoyrpdlidwxcsgvekjgajrvdspdxgvrxikdipkazpoyrauavnnnldrovwnojmktbcdqntsfhbqpyomksbnqdejknwnwtoeksdyefngtsrcgaifvpgqxwmqklbsjqzqpxmbmyxuguosoliixhlenxzfgglspywsanphzebcxizawuiwaifmtnjzmvsruidisojmeqnsdaqxvzthangnwpnfqzbbycgmkttflqatvravelihczkldqcovxcdbezqumjcsywftmfagnuvkbqatahmlinnuogpqgrxxgprsyrulfjiwdwhzptkmjskpqmpuardgyzozdjdkfihuihrplomumsbuqkisyavmwsvnghefeqlhsiaddcfbtrkekulecuntzdoievfdrqctnfirsyahbdnsejntkvliqlrpsxldvfkkpmpcnxnsdwwdesqlfbgxdscisqhcrdcirfyilzmdrefyouyjitpikodyvbrzswimouzamtgktvhzuefthqvwgvcmcytkkhmkkekfoyxagpjcpmzepduhfvzfpfvtxgzkbspkctudujbgciysixdomepkigsfwluzuhjqymmzxktyxwbxuwveksqcvlzojiomtplhukbebfulpinwihsqinwwmfogfmkreenvmywgmpytdcpflkeznwalpuusgzgpjwyukmlxhohlayqcmdlubuzuvfagkkdnmgomxcciexkqcjrsvhgmcdvpmekrcibsjamcszgsqmykhnyuuzwlqyxqdjgmuzkwpzgwjspromixunautatwoybqethddhnuzeoutoijrraitqmoqkiceviftcmmcgemwmbpquwjheogbuexupbxnqhfdwjciejjrkaovijgmokeavlsrdnniorhaoteersvfaqjkukgilartvjzhqqdpbuguyxjyiqgiqkqqo";
+var delayInMilliseconds = 10;
 
-const SOCKETFILE = "/usr/local/temp/socket_test";
+//unix file system
+var SOCKETFILE;
+if(process.platform == "win32") {
+    SOCKETFILE = "C:\\tmp\\sock";
+} else {
+    SOCKETFILE = "/tmp/socket_test";
+}
+
 var counter = 0;
+
+
 var client = net.createConnection(SOCKETFILE)
     .on("connect", () => {
         console.log("Client Connected");
@@ -14,14 +24,20 @@ var client = net.createConnection(SOCKETFILE)
         console.log("Recieved From Server");
     })
     ;
+client.write(RANDOMSTRING);
 
-for(var i = 0; i < 10000; i++) {
+for(var q = 0; q < 1000; q++) {
     client.write(RANDOMSTRING);
 }
 
-for(var i = 0; i < 1000000; i++) {
-
-}
+setTimeout(
+    function() {
+        for (var i = 0; i < 10000; i++) {
+            setTimeout(function () {
+                client.write(RANDOMSTRING);
+            }, delayInMilliseconds * i);
+        }
+    }, 2000);
 
 console.log("Send 10,000 transmissions, got back: ");
 console.log(counter);
