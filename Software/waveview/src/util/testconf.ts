376981f29d2195e3ab293cf9ed42ce266d01d9e3
Ratan Varghese <ratanv@protonmail.com>
2021-03-18 01:02:31 -0400

    Add testconf

---
+++ Software/waveview/src/util/testconf.ts
@@ -0,0 +1,38 @@
+import { CMD, PlumberArgs, Plumber } from './plumber';
+
+class TestConf {
+  getChArgs: PlumberArgs;
+  setChArgs: PlumberArgs;
+
+  constructor() {
+    this.getChArgs = {
+      headCheck: () => true,
+      bodyCheck: (a, bytesRead, body) => {
+        console.log(body[0]);
+        return true;
+      },
+      cmd: CMD.CMD_GetCh,
+      id: 0,
+      writeData: []
+    }
+
+    this.setChArgs = {
+      headCheck: () => true,
+      bodyCheck: () => true,
+      cmd: CMD.CMD_SetCh,
+      id: 0,
+      writeData: [4, 0]
+    }
+  }
+
+  update(get: boolean) {
+    if(get) {
+      Plumber.getInstance().cycle(this.getChArgs);
+    }
+    else {
+      Plumber.getInstance().cycle(this.setChArgs);
+    }
+  }
+}
+
+export default TestConf;
