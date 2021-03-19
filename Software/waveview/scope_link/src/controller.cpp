bab625b3e07dc380c3526acf20b08dffce4f0c72
Ratan Varghese <ratanv@protonmail.com>
2021-03-19 12:00:27 -0400

    move stuff to hpp

--- Software/waveview/scope_link/src/controller.cpp
+++ Software/waveview/scope_link/src/controller.cpp
@@ -2,38 +2,8 @@
 #include "logger.hpp"
 
 //RampDemo Related
-#define RD_DATA_PER_CHAN 1024
-#define RD_CHAN_COUNT 4
-#define RD_PACKET_SIZE 4096
-int8_t RD_PACKET_ORIGINAL[RD_PACKET_SIZE]; 
+int8_t RD_PACKET_ORIGINAL[RD_PACKET_SIZE];
 
-
-enum CMD {
-    //Data commands
-    CMD_GetData1 = 0x01,
-    CMD_GetData2 = 0x02,
-    CMD_GetData3 = 0x03,
-    CMD_GetData4 = 0x04,
-
-    //Demo commands
-    CMD_SetFile = 0x11,
-    CMD_RampDemo = 0x1F,
-
-    //Get Config commands
-    CMD_GetWindowSize = 0x21,
-    CMD_GetCh = 0x22,
-    CMD_GetLevel = 0x23,
-    CMD_GetTriggerCh = 0x24,
-    CMD_GetEdgeType = 0x25,
-
-    //Set Config commands
-    CMD_SetWindowSize = 0x31,
-    CMD_SetCh = 0x32,
-    CMD_SetLevel = 0x33,
-    CMD_SetTriggerCh = 0x34,
-    CMD_SetEdgeType = 0x35,
-    CMD_SetMath = 0x3F
-};
 
 controller::controller(boost::lockfree::queue<buffer*, boost::lockfree::fixed_sized<false>> *inputQ)
 {
