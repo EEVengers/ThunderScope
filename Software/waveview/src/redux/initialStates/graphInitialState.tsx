099ae0dcd476aad2cf92b00ac73c69acd1b25cdb
Jason Bonnell <jbonnell@uwaterloo.ca>
2021-03-21 15:44:50 -0400

    mapped xdoman/ydomain to graph redux

--- Software/waveview/src/redux/initialStates/graphInitialState.tsx
+++ Software/waveview/src/redux/initialStates/graphInitialState.tsx
@@ -1,7 +1,9 @@
 import GraphStatus from "../../configuration/enums/graphStatus";
 
 const GraphInitialState = {
-  currentStatus: GraphStatus.On
+  currentStatus: GraphStatus.On,
+  xDomain: [0,0],
+  yDomain: [0,0]
 };
 
 export default GraphInitialState;
