96b46c3ac4a445ebd1ed5de138919e7b9b2f1cf9
Ratan Varghese <ratanv@protonmail.com>
2021-03-27 02:30:33 -0400

    Run C++ inside Electron

--- Software/waveview/electron/preload.ts
+++ Software/waveview/electron/preload.ts
@@ -2,17 +2,39 @@
 // It has the same sandbox as a Chrome extension.
 
 import * as fs from 'fs';
-import { exec } from 'child_process';
+import { spawn } from 'child_process';
 import { contextBridge } from 'electron';
 
+const cwd = process.cwd();
+const cpp = spawn(cwd + "\\build_cpp\\Release\\scope.exe", ["-c"]);
+
+var did_open = false;
 const SOCKET_PREFIX = (process.platform == "win32") ? "\\\\.\\pipe\\" : "/tmp/";
 const SOCKETFILE_TX = SOCKET_PREFIX + "testPipeRX";
 const SOCKETFILE_RX = SOCKET_PREFIX + "testPipeTX";
-
 var TX_FD = -1;
 var RX_FD = -1;
-fs.open(SOCKETFILE_TX, "w", (err, fd) => {TX_FD = fd;});
-fs.open(SOCKETFILE_RX, "r", (err, fd) => {RX_FD = fd;});
+
+cpp.stdout.on('data', (data) => {
+  console.log(`cpp stdout: ${data}`);
+  if(TX_FD != -1 && RX_FD != 1) {
+    did_open = true;
+  }
+  if(!did_open) {
+    fs.open(SOCKETFILE_TX, "w", (err, fd) => {
+      if(err) {
+        console.error(err);
+      }
+      TX_FD = fd;
+    });
+    fs.open(SOCKETFILE_RX, "r", (err, fd) => {
+      if(err) {
+        console.error(err);
+      }
+      RX_FD = fd;
+    });
+  }
+})
 
 //Welcome to the future: https://www.electronjs.org/docs/tutorial/context-isolation
 contextBridge.exposeInMainWorld("thunderBridge", {
