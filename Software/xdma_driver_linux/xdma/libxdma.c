b29db43d896b9c0d9e3ceb327de70b40e50534cf
John Simons <jammsimons@gmail.com>
2024-05-16 01:09:46 +0200

    azonenberg kernel backport fixes

--- Software/xdma_driver_linux/xdma/libxdma.c
+++ Software/xdma_driver_linux/xdma/libxdma.c
@@ -2908,8 +2908,15 @@ static void transfer_destroy(struct xdma_dev *xdev, struct xdma_transfer *xfer)
 		struct sg_table *sgt = xfer->sgt;
 
 		if (sgt->nents) {
+
+#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 16, 0)
 			pci_unmap_sg(xdev->pdev, sgt->sgl, sgt->nents,
 				     xfer->dir);
+#else
+			dma_unmap_sg(&xdev->pdev->dev, sgt->sgl, sgt->nents,
+				     xfer->dir);
+#endif
+
 			sgt->nents = 0;
 		}
 	}
@@ -3192,8 +3199,16 @@ ssize_t xdma_xfer_aperture(struct xdma_engine *engine, bool write, u64 ep_addr,
 	}
 
 	if (!dma_mapped) {
+
+
+#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 16, 0)
 		sgt->nents = pci_map_sg(xdev->pdev, sgt->sgl, sgt->orig_nents,
 					dir);
+#else
+		sgt->nents = dma_map_sg(&xdev->pdev->dev, sgt->sgl,
+					sgt->orig_nents, dir);
+#endif
+
 		if (!sgt->nents) {
 			pr_info("map sgl failed, sgt 0x%p.\n", sgt);
 			return -EIO;
@@ -4191,18 +4206,31 @@ static int set_dma_mask(struct pci_dev *pdev)
 
 	dbg_init("sizeof(dma_addr_t) == %ld\n", sizeof(dma_addr_t));
 	/* 64-bit addressing capability for XDMA? */
-	if (!pci_set_dma_mask(pdev, DMA_BIT_MASK(64))) {
+#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 16, 0)
+	if (!pci_set_dma_mask(pdev, DMA_BIT_MASK(64))) 
+#else
+	if (!dma_set_mask_and_coherent(&pdev->dev, DMA_BIT_MASK(64))) 
+#endif
+	{
 		/* query for DMA transfer */
 		/* @see Documentation/DMA-mapping.txt */
-		dbg_init("pci_set_dma_mask()\n");
+		dbg_init("set_dma_mask(64)\n");
+#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 16, 0)
+		pci_set_consistent_dma_mask(pdev, DMA_BIT_MASK(64));
+#endif
 		/* use 64-bit DMA */
 		dbg_init("Using a 64-bit DMA mask.\n");
-		/* use 32-bit DMA for descriptors */
-		pci_set_consistent_dma_mask(pdev, DMA_BIT_MASK(32));
-		/* use 64-bit DMA, 32-bit for consistent */
-	} else if (!pci_set_dma_mask(pdev, DMA_BIT_MASK(32))) {
+	} else 
+#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 16, 0)
+	if (!pci_set_dma_mask(pdev, DMA_BIT_MASK(32))) 
+#else
+	if (!dma_set_mask_and_coherent(&pdev->dev, DMA_BIT_MASK(32))) 
+#endif
+	{
 		dbg_init("Could not set 64-bit DMA mask.\n");
+#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 16, 0)
 		pci_set_consistent_dma_mask(pdev, DMA_BIT_MASK(32));
+#endif
 		/* use 32-bit DMA */
 		dbg_init("Using a 32-bit DMA mask.\n");
 	} else {
