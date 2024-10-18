0778f849d53dd039162b4447daf34f1aa71f3b59
Aleksa Bjelogrlic <aleksa@eevengers.com>
2024-10-18 15:46:18 -0400

    Only use .bin for flashing

--- Gateware/flash_latest_prebuilt.sh
+++ Gateware/flash_latest_prebuilt.sh
@@ -1,18 +1,15 @@
 mkdir prebuilt_bitstreams
 cd prebuilt_bitstreams
 wget https://github.com/EEVengers/thunderscope_litex/releases/download/beta_2-0fd27d5b/thunderscope_a100t.bin -O thunderscope_a100t.bin
-wget https://github.com/EEVengers/thunderscope_litex/releases/download/beta_2-0fd27d5b/thunderscope_a100t.bit -O thunderscope_a100t.bit
 wget https://github.com/EEVengers/thunderscope_litex/releases/download/beta_2-0fd27d5b/thunderscope_a200t.bin -O thunderscope_a200t.bin
-wget https://github.com/EEVengers/thunderscope_litex/releases/download/beta_2-0fd27d5b/thunderscope_a200t.bit -O thunderscope_a200t.bit
 wget https://github.com/EEVengers/thunderscope_litex/releases/download/beta_2-0fd27d5b/thunderscope_a50t.bin -O thunderscope_a50t.bin
-wget https://github.com/EEVengers/thunderscope_litex/releases/download/beta_2-0fd27d5b/thunderscope_a50t.bit -O thunderscope_a50t.bit
 
 if [ "$1" == "a50t" ]; then
-	openFPGALoader --fpga-part xc7a50tcsg324 -f -c digilent_hs2 thunderscope_a50.bit
+	openFPGALoader --fpga-part xc7a50tcsg324 -f -c digilent_hs2 thunderscope_a50.bin
 elif [ "$1" == "a100t" ]; then
-	openFPGALoader --fpga-part xc7a100tfgg484 -f -c digilent_hs2 thunderscope_a100.bit
+	openFPGALoader --fpga-part xc7a100tfgg484 -f -c digilent_hs2 thunderscope_a100.bin
 elif [ "$1" == "a100t" ]; then
-	openFPGALoader --fpga-part xc7a200tfbg484 -f -c digilent_hs2 thunderscope_a200.bit
+	openFPGALoader --fpga-part xc7a200tfbg484 -f -c digilent_hs2 thunderscope_a200.bin
 else
 	echo "Valid arguments are: a50t a100t a200t"
 fi
