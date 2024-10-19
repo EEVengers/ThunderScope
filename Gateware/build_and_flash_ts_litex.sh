a2d04bce3a6c6ded48bf1e04909e46710d0e97f5
Aleksa Bjelogrlic <aleksa@eevengers.com>
2024-10-18 22:31:01 -0400

    Changed --load to --flash to flash qspi

--- Gateware/build_and_flash_ts_litex.sh
+++ Gateware/build_and_flash_ts_litex.sh
@@ -9,11 +9,11 @@ source ~/Xilinx/Vivado/2024.1/settings64.sh
 export LITEX_ENV_VIVADO=~/Xilinx/Vivado/2024.1
 
 if [ "$1" == "a50t" ]; then
-	./thunderscope.py --variant=a50t --build --load
+	./thunderscope.py --variant=a50t --build --flash
 elif [ "$1" == "a100t" ]; then
-	./thunderscope.py --variant=a100t --build --load
+	./thunderscope.py --variant=a100t --build --flash
 elif [ "$1" == "a200t" ]; then
-	./thunderscope.py --variant=a200t --build --load
+	./thunderscope.py --variant=a200t --build --flash
 else
 	echo "Valid arguments are: a50t a100t a200t"
 fi
