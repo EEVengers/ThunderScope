d473653d3e3808038d49908828e2205a4922093b
Aleksa Bjelogrlic <aleksa@eevengers.com>
2024-10-18 21:12:12 -0400

    Updated script to source Vivado

--- Gateware/build_and_flash_ts_litex.sh
+++ Gateware/build_and_flash_ts_litex.sh
@@ -1,5 +1,13 @@
 git clone https://github.com/EEVengers/thunderscope_litex.git
 cd thunderscope_litex
+
+#https://adaptivesupport.amd.com/s/article/76585?language=en_US
+sudo apt install libtinfo-dev
+sudo ln -s /lib/x86_64-linux-gnu/libtinfo.so.6 /lib/x86_64-linux-gnu/libtinfo.so.5
+
+source ~/Xilinx/Vivado/2024.1/settings64.sh 
+export LITEX_ENV_VIVADO=~/Xilinx/Vivado/2024.1
+
 if [ "$1" == "a50t" ]; then
 	./thunderscope.py --variant=a50t --build --load
 elif [ "$1" == "a100t" ]; then
