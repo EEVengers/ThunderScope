8475c29d0c4afdbf2f0b7c2b8df344eb2f055f1c
Aleksa Bjelogrlic <aleksa@eevengers.com>
2025-03-13 12:47:15 -0400

    Got multi-channel routing working
    Needed to change refdes on front end channel components

--- Hardware/KiCad/Thunderscope_Rev5/per_channel_designators.py
+++ Hardware/KiCad/Thunderscope_Rev5/per_channel_designators.py
@@ -2,39 +2,29 @@
 input_filename = "FE_Channel.kicad_sch"
 output_filename = "FE_Channel_Copy.kicad_sch"
 
-instance_spacing = 3
-per_channel_spacing = 4
-num_channels = 4
-designator_suffix_order = ["B","A","C","D"]
-
-instance_counter = 0
-per_channel_counter = 0
-num_channel_counter = 0
+designator_suffix_order = ["2","1","3","4"]
 designator = ""
+is_valid = False
+num_channels = 4
+channel_count = 0
 
 # Open the file and read its content.
 with open(output_filename, "w") as output_file:
     with open(input_filename) as input_file:
         for line in input_file:
-            if "(instances" in line:
-                instance_counter = instance_spacing
-            elif instance_counter > 1:
-                instance_counter -= 1
-            elif instance_counter == 1:
-                instance_counter -= 1
-                if "#PWR?" not in line:
-                    designator = line.split('"')[1]
-                    num_channel_counter = num_channels
-                    line = (line.split('"')[0] + '"' + designator + designator_suffix_order[4-num_channel_counter] + '"' + line.split('"')[2])
-                    per_channel_counter = per_channel_spacing
-                    print (line)
-            elif per_channel_counter > 1:
-                per_channel_counter -= 1
-            elif per_channel_counter == 1:
-                per_channel_counter -= 1
-                if (num_channel_counter > 1):
-                    num_channel_counter -= 1
-                    per_channel_counter = per_channel_spacing
-                    line = (line.split('"')[0] + '"' + designator + designator_suffix_order[4-num_channel_counter] + '"' + line.split('"')[2])
-                print (line)
+            match line:
+                case x if x.startswith('		(property "Reference"'):
+                    reference_old = line.split('"')[3]
+                    print(reference_old)
+                    if "#PWR" not in reference_old:
+                        reference_new = reference_old[:-2] + "_1"
+                        print (reference_new)
+                        line = line.split(reference_old)[0] + reference_new +  line.split(reference_old)[1]
+                        channel_count = num_channels
+                case x if x.startswith('					(reference'):
+                    if channel_count > 0:
+                        reference_old = line.split('"')[1]
+                        line = line.split(reference_old)[0] + reference_new[:-1] + designator_suffix_order[4-channel_count] +  line.split(reference_old)[1]
+                        channel_count -= 1
+                        print (line)
             output_file.write(line)
