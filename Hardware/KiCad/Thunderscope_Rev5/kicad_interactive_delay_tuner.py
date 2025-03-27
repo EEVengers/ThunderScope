2b4dbd54a76e1ae29f64d50d680ea223218fae7f
Aleksa Bjelogrlic <aleksa@eevengers.com>
2025-03-27 18:46:35 -0400

    Got delay tuner to write to rules file

--- Hardware/KiCad/Thunderscope_Rev5/kicad_interactive_delay_tuner.py
+++ Hardware/KiCad/Thunderscope_Rev5/kicad_interactive_delay_tuner.py
@@ -7,16 +7,22 @@ from kipy.util.units import to_mm
 
 er_per_layer = [2.78,-1,3.66,3.66,-1,2.78]
 via_prop_delay_ps = 23.09 # sqrt(Cvia * Lvia)
+board_height_mm = 1.6
 mm_per_m = 1000.0
 s_per_ps = 1e-12
 c = 2.998e8
 
 def main():
     board_file = "Thunderscope_Rev5.kicad_pcb"
+    rules_file = "Thunderscope_Rev5.kicad_dru"
     vivado_io_csv_file = "impl_1.csv"
+    
+    refresh = False
     time_modified_old = 0
     time_modified_new = 0
     
+    equiv_len_max = 0
+
     try:
         kicad = KiCad()
     except BaseException as e:
@@ -24,19 +30,27 @@ def main():
         exit()
 
     pad_delay_dict = get_pad_delays(vivado_io_csv_file)
-    calc_net_delays(kicad,"LVDS_ADC",pad_delay_dict)
-
-"""     if (os.path.isfile(board_file)):
-        try:
-            while True: 
-                time_modified_new = os.path.getmtime(board_file)
-                if (time_modified_new > time_modified_old):
-                    calc_delays(kicad)
-                    time_modified_old = time_modified_new
-                time.sleep(1)
-                        
-        except KeyboardInterrupt:
-            pass """
+    
+    if (refresh == False):
+        equiv_len_max,per_net_delay_dict = calc_net_delays(kicad,"LVDS_ADC",pad_delay_dict)
+        print ("Max Equiv Length: " + str(equiv_len_max))
+        user_write_prompt = input("Write Rules File (y/n): ")
+        if (user_write_prompt == "y"):
+            write_rule_file(per_net_delay_dict,rules_file)
+    else:
+        if (os.path.isfile(board_file)):
+            try:
+                while True: 
+                    time_modified_new = os.path.getmtime(board_file)
+                    if (time_modified_new > time_modified_old):
+                        equiv_len_max_new,per_net_delay_dict = calc_net_delays(kicad,"LVDS_ADC",pad_delay_dict)
+                        if (equiv_len_max_new <= equiv_len_max_old):
+                            write_rule_file(per_net_delay_dict,rules_file)
+                        time_modified_old = time_modified_new
+                    time.sleep(1)
+                            
+            except KeyboardInterrupt:
+                pass
 
 
 def get_pad_delays(vivado_io_csv_file):
@@ -130,8 +144,8 @@ def calc_net_delays(kicad,netclass,pad_delay_dict):
             if (pad.net == nets[net_index]):
                 if (pad.number in pad_delay_dict.keys()):
                     per_net_stats[net_index][num_cu_layers + 1] = pad_delay_dict[pad.number]
-                    print (pad.number)
-                    print (pad_delay_dict[pad.number])
+                    #print (pad.number)
+                    #print (pad_delay_dict[pad.number])
     
     # Step 7
     # Calc the ps_per_mm scaling factor
@@ -150,18 +164,77 @@ def calc_net_delays(kicad,netclass,pad_delay_dict):
     
     for net_index in range (num_nets):
         
+        per_net_length_kicad = 0
+        per_net_length_equiv = 0
         per_net_delay = 0
         
+        # Traces
         for layer_index in range (num_cu_layers):
+            per_net_length_kicad += per_net_stats[net_index][layer_index]
             per_net_delay += per_net_stats[net_index][layer_index] * ps_per_mm_by_layer[layer_index]
+        
+        # Vias
+        per_net_length_kicad += per_net_stats[net_index][num_cu_layers] * board_height_mm
         per_net_delay += per_net_stats[net_index][num_cu_layers] * via_prop_delay_ps    
+        
+        # Pad Delay
         per_net_delay += per_net_stats[net_index][num_cu_layers + 1]
+        
+        # Convert Pad Delay to Length for Equiv
+        per_net_length_equiv = per_net_stats[net_index][num_cu_layers + 1] / ps_per_mm_by_layer[0]
+        per_net_length_equiv += per_net_length_kicad
 
-        per_net_delay_dict[nets[net_index].name] = per_net_delay
+        # Dictionary
+        per_net_delay_dict[nets[net_index].name] = [per_net_length_kicad,per_net_delay,per_net_length_equiv]
+    
+    # Step 9
+    # Find net with greatest delay, then make use equivalent length deltas to set Length Rules  
+    # For each net: Length Rule = Kicad Length + (Equiv Length of max delay net  - Equiv Length)
 
+    equiv_len_max = 0
+
+    for x in per_net_delay_dict.values():
+        if x[2] > equiv_len_max:
+            equiv_len_max = x[2]
+
+    for x in per_net_delay_dict.values():
+        x.append(x[0] + equiv_len_max - x[2])
+
+    print ("-"*113)
+    print ("| Net\t\t| Kicad Length (mm)\t| Total Delay (ps)\t| Equiv. Length (mm)\t| Rule Length (mm)\t|")
+    print ("-"*113)
     for x, y in per_net_delay_dict.items():
-        print(x, y)
+        print("| " + x + "\t| " + str(y[0]) + "\t| " + str(y[1]) + "\t| " + str(y[2]) + "\t| " + str(y[3]) + "\t|" )
+    print ("-"*113)
+
+    return equiv_len_max,per_net_delay_dict
 
 
+def write_rule_file(per_net_delay_dict,rules_file):
+    
+    with open(rules_file) as f:
+        lines = f.readlines()
+    
+    line_num_start = -1
+
+    for x in range(len(lines)):
+        if lines[x].startswith("# DELAY TUNER RULES"):
+            line_num_start = x
+    
+    if (line_num_start != -1):
+        with open(rules_file, "w") as f:
+            for x in range(len(lines)):
+                f.write(lines[x])
+                if (x == line_num_start):
+                    break
+            f.write("\n")
+            for x,y in per_net_delay_dict.items():
+                f.write('(rule "' + x + ' length"\n')
+                f.write('\t(condition "A.NetName == \'' + x + '\'")\n')
+                f.write("\t(constraint length ")
+                f.write("(min " +  str(y[3]) + "mm) ")
+                f.write("(opt " +  str(y[3]) + "mm) ")
+                f.write("(max " +  str(y[3]) + "mm)))\n")
+                
 if __name__ == '__main__':
     main()    
